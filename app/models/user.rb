bcde8d6561503302159eea1d4043cb69c8788431
inaken12 <12halcyon12@gmail.com>
2021-01-21 15:29:22 +0900

    バリデーションの追加設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Han}]+\z/, message: "Full-width characters" } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers" }
 end
