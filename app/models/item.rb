5b237139b831f3bab399c96a836c9cbc8ebb4d4b
inaken12 <12halcyon12@gmail.com>
2021-01-22 17:33:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,15 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :state
   belongs_to :shipping_day
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :state_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' },
+                                    format: { with: ( numericality: { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 } ), message: 'Out of setting range' }
 end
