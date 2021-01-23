e52332e0bf0fab48931533d9318b7046130a2fbf
inaken12 <12halcyon12@gmail.com>
2021-01-23 14:47:20 +0900

    販売手数料・販売利益が自動計算されるように表示を変更

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+function itemPrice () {
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = String(Math.floor(inputValue * 0.1)).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = String(Math.floor(inputValue - inputValue * 0.1)).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+  });
+}
+
+window.addEventListener ('load', itemPrice);
