3b33385ebdcce31de69ba0a9dadb434d419c6b59
inaken12 <12halcyon12@gmail.com>
2021-01-23 18:54:50 +0900

    エラーハンドリングの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,15 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_fee_id, numericality: { other_than: 1 }
-  validates :state_id, numericality: { other_than: 1 }
-  validates :shipping_day_id, numericality: { other_than: 1 }
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :state_id
+    validates :shipping_day_id
+  end
+  
   validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
 end
