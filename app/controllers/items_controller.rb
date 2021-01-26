8537557f7eb521531ba088239644ff8ce80c6fdf
inaken12 <12halcyon12@gmail.com>
2021-01-26 14:43:54 +0900

    ログイン状態の出品者のみ商品削除ができるよう設定2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,13 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if current_user.id == item.user_id
-      item.destroy
-      redirect_to root_path
-    else
-      redirect_to action: :index
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
