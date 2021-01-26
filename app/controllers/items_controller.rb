11829afd39e9b81bd689bf25ff0c581bca665d43
inaken12 <12halcyon12@gmail.com>
2021-01-26 14:29:01 +0900

    削除に失敗した場合の条件分岐の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,6 +39,8 @@ class ItemsController < ApplicationController
     if current_user.id == item.user_id
       item.destroy
       redirect_to root_path
+    else
+      redirect_to action: :index
     end
   end
 
